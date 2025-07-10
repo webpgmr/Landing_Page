768fcc61232836447ecee5f15a825469fb536929
Bijeesh Balan <bijeesh.balanr@inspiring-travel.de>
2025-07-10 19:10:58 +0530

    gitignore added

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        'orange-btn': "#F57C00",
+      },
+      screens: {
+        '2xl': '1728px',
+        'xl': '1280px',
+        'lg': '1024px',
+        'md': '768px',
+        'sm': '480px',
+      },
+    },
+  },
+  plugins: [],
+}
